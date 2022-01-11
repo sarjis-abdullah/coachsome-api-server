f779da3a1f5bbdf3f40ed2066c9c9eb304360fb4
hafij <hafijur@tikweb.dk>
2022-01-11 12:46:05 +0600

    Fixed currency fallback issue

--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -6,6 +6,7 @@ namespace App\Services;
 
 use App\Entities\Currency;
 use App\Entities\CurrencyRate;
+use App\Utils\CurrencyUtil;
 use Carbon\Carbon;
 use Exception;
 
@@ -25,86 +26,12 @@ class CurrencyService
      */
     public function convert($amount, $fromCurrencyCode, $toCurrencyCode, $date = null)
     {
-        $rate = null;
-        $isHistory = false;
-        $latestDate = null;
-        $historyDate = null;
-        $result = null;
-
-        if ($date) {
-            $isHistory = true;
-            $historyDate = Carbon::parse($date)->format("Y-m-d");
-        } else {
-            $latestDate = Carbon::now()->format("Y-m-d");
-        }
-        if ($isHistory) {
-            $currencyRate = CurrencyRate::where('base', $fromCurrencyCode)
-                ->where("date", $historyDate)
-                ->first();
-            if (!$currencyRate) {
-                $rates = $this->rates($fromCurrencyCode, $historyDate);
-                $currencyRate = new CurrencyRate();
-                $currencyRate->base = $fromCurrencyCode;
-                $currencyRate->date = $historyDate;
-                $currencyRate->rates = json_encode($rates);
-                $currencyRate->save();
-            }
-            $rates = json_decode($currencyRate->rates, true);
-            $rate = $rates[$toCurrencyCode];
-        } else {
-            $currencyRate = CurrencyRate::where('base', $fromCurrencyCode)
-                ->where("date", $latestDate)
-                ->first();
-            if (!$currencyRate) {
-                $rates = $this->rates($fromCurrencyCode);
-                $currencyRate = new CurrencyRate();
-                $currencyRate->base = $fromCurrencyCode;
-                $currencyRate->date = $latestDate;
-                $currencyRate->rates = json_encode($rates);
-                $currencyRate->save();
-            }
-            $rates = json_decode($currencyRate->rates, true);
-            $rate = $rates[$toCurrencyCode];
-        }
-
-        $result = $amount * $rate;
-
-        return $result;
+        return CurrencyUtil::convert($amount, $fromCurrencyCode, $toCurrencyCode, $date = null);
     }
 
     public function rates($base, $date = null)
     {
-        $data = [];
-
-        try {
-            // Exchange rate host
-            if ($date) {
-                $req_url = 'https://api.exchangerate.host/' . $date . '?base=' . $base;
-            } else {
-                $req_url = 'https://api.exchangerate.host/latest?base=' . $base;
-            }
-            $response_json = file_get_contents($req_url);
-            $response = json_decode($response_json, true);
-            $data = $response["rates"];
-        } catch (Exception $e) {
-            // Fallback free currency api
-            throw new Exception("Error");
-            if ($date) {
-                $req_url = "https://freecurrencyapi.net/api/v2/historical?apikey=78e27350-721e-11ec-a972-5f85bd619f72&base_currency="
-                    . $base
-                    . "&date_from="
-                    . $date
-                    . "&date_to="
-                    . $date;
-            } else {
-                $req_url = "https://freecurrencyapi.net/api/v2/latest?apikey=78e27350-721e-11ec-a972-5f85bd619f72&base_currency=" . $base;
-            }
-            $response_json = file_get_contents($req_url);
-            $response = json_decode($response_json, true);
-            $data = $response["data"];
-        }
-
-        return $data;
+        return CurrencyUtil::rates($base, $date);
     }
 
     public function format($amount, $code)
