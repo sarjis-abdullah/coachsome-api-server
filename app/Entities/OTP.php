18fc4b9439eb3691b085c6e5e497cfed867ead4b
masudul <masudul@tikweb.dk>
2022-01-26 11:55:50 +0000

    added OTP validation method for PWA

---
+++ app/Entities/OTP.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OTP extends Model
+{
+    use HasFactory;
+    protected $table = "otp_verifications";
+
+    protected $fillable = [
+        'email',
+        'otp',
+    ];
+}
