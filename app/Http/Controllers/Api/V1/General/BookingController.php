43399897561928b161f3fa50590a6a10db7aa837
masudul <masudul@tikweb.dk>
2022-04-21 13:49:54 +0600

    fix payment card metadata issue

--- app/Http/Controllers/Api/V1/General/BookingController.php
+++ app/Http/Controllers/Api/V1/General/BookingController.php
@@ -106,7 +106,7 @@ class BookingController extends Controller
             if ($mPaymentCard) {
                 $quickpayCardService = new QuickpayCardService();
                 $paymentCardObj = $quickpayCardService->getQuickPayCard($mPaymentCard->card_id)->asObject();
-                $paymentCard = $paymentCardObj ? $paymentCardObj->metadata : null;
+                $paymentCard = $paymentCardObj && $paymentCardObj->metadata ? $paymentCardObj->metadata : null;
             }
 
             // Package charge info
