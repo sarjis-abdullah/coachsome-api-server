2018eb212c4db567619d97ac2bcded3ffa9e8d80
sarjis-abdullah0 <sarjis-abdullah@gmail.com>
2022-06-17 17:13:23 +0600

    contact user duplication removed

--- app/Console/Commands/CreateContactUserFromExistingUser.php
+++ app/Console/Commands/CreateContactUserFromExistingUser.php
@@ -6,6 +6,7 @@ use App\Entities\Contact;
 use App\Entities\ContactUser;
 use App\Entities\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class CreateContactUserFromExistingUser extends Command
 {
@@ -30,34 +31,49 @@ class CreateContactUserFromExistingUser extends Command
      */
     public function handle()
     {
+//        $this->info('Process in progress ...');
+//        $users = User::all();
+//        foreach ($users as $user){
+//            $contacts = Contact::where('user_id', $user->id)->get();
+//            foreach ($contacts as $contact){
+//                $connectionUser = User::find($contact->connection_user_id);
+//                if ($connectionUser){
+//                    $hasContactUser = ContactUser::where('email', '=', $connectionUser->email)
+//                        ->where('receiverUserId', '=', $user->id)
+//                        ->first();
+//                    if ($hasContactUser){
+//                        continue;
+//                    }
+//                }
+//
+//                if ($connectionUser){
+//                    $c['contactAbleUserId'] = $contact->connection_user_id;
+//                    $c['receiverUserId'] = $user->id;
+//                    $c['firstName'] = $connectionUser->first_name;
+//                    $c['lastName'] = $connectionUser->last_name;
+//                    $c['email'] = $connectionUser->email;
+//                    $c['comment'] = "Created from existing user";
+//                    $c['lastActiveAt'] = \Carbon\Carbon::now();
+//                    ContactUser::create($c);
+//                }
+//            }
+//        }
+//
+//        $this->info('Created contact user successfully!');
         $this->info('Process in progress ...');
-        $users = User::all();
-        foreach ($users as $user){
-            $contacts = Contact::where('user_id', $user->id)->get();
-            foreach ($contacts as $contact){
-                $connectionUser = User::find($contact->connection_user_id);
-                if ($connectionUser){
-                    $hasContactUser = ContactUser::where('email', '=', $connectionUser->email)
-                        ->where('receiverUserId', '=', $user->id)
-                        ->first();
-                    if ($hasContactUser){
-                        continue;
-                    }
-                }
+        $duplicateRecords = DB::table('contact_users')
+            ->select('email', DB::raw('count(`email`) as occurrences'))
+            ->groupBy('email')
+            ->having('occurrences', '>', 1)
+            ->get();
 
-                if ($connectionUser){
-                    $c['contactAbleUserId'] = $contact->connection_user_id;
-                    $c['receiverUserId'] = $user->id;
-                    $c['firstName'] = $connectionUser->first_name;
-                    $c['lastName'] = $connectionUser->last_name;
-                    $c['email'] = $connectionUser->email;
-                    $c['comment'] = "Created from existing user";
-                    $c['lastActiveAt'] = \Carbon\Carbon::now();
-                    ContactUser::create($c);
-                }
-            }
+        foreach ($duplicateRecords as $duplicate) {
+            // Get the row you don't want to delete.
+            $dontDeleteThisRow = DB::table('contact_users')->where('email', '=', $duplicate->email)->first();
+
+            // Delete all rows except the one we fetched above.
+            $dontDeleteThisRow && DB::table('contact_users')->where('email', '=', $duplicate->email)->where('id', '!=', $dontDeleteThisRow->id)->delete();
         }
-
-        $this->info('Created contact user successfully!');
+        $this->info('Process ends');
     }
 }
